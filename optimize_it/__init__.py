a24fdc9eed07b0bd8ff2efeea3e6f85c9a87a6bc
Gustavo Nagel <gust.nagel@gmail.com>
2023-07-02 18:24:53 -0300

    💥 Add template commit

---
+++ optimize_it/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""Awesome `optimize-it` is a Python cli/package with optimization tools"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
